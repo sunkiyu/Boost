c729b736cd7eaceb8a018c520c8298df3bdea410
sunkiyu <68372094+sunkiyu@users.noreply.github.com>
2022-05-12 18:01:52 +0900

    Create CorePch.h

---
+++ BoostServer/CorePch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#include "boost/asio.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+#include "Types.h"
+#include "Container.h"
+#include <thread>
+
+using namespace std;
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+
